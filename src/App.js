6ad334d9e0e5202b36b6297a870aa60a0ac08e79
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-04-14 09:10:51 +0600

    css add

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import News from './News/News';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [article,setArticle]= useState([]);
+  useEffect(()=>{
+    fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-03-14&sortBy=publishedAt&apiKey=a757074d6a164334a170b27bb8a57562")
+    .then(res=>res.json())
+    .then(data=>setArticle(data.articles))
+  })
   return (
     <div>
-    
+      <h2>total News : {article.length}</h2>
+    {
+      article.map(artcle=><News artcle={artcle} ></News>)
+    }
       
-      <News></News>
+      
       
 
     </div>
--- src/News/News.js
+++ src/News/News.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import "./News.css"
 
 const useStyles = makeStyles({
   root: {
@@ -16,24 +17,24 @@ const useStyles = makeStyles({
     height: 140,
   },
 });
-const News = () => {
+const News = (props) => {
+  const{author,urlToImage,description}=props.artcle
     const classes = useStyles();
     return (
-        <div>
+        <div className="articles">
             <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={urlToImage}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            {author}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>

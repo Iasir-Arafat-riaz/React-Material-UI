bbe38aae8452e88c118b078d94ac68789e9cdd55
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-04-16 06:42:12 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import News from './News/News';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
   const [article,setArticle]= useState([]);
+  // useEffect(()=>{
+  //   fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-03-15&sortBy=publishedAt&apiKey=a757074d6a164334a170b27bb8a57562")
+  //   .then(res=>res.json())
+  //   .then(data=>setArticle(data.articles))
+  // },[]);
   useEffect(()=>{
-    fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-03-15&sortBy=publishedAt&apiKey=a757074d6a164334a170b27bb8a57562")
-    .then(res=>res.json())
-    .then(data=>setArticle(data.articles))
-  },[]);
+    axios("https://newsapi.org/v2/everything?q=tesla&from=2021-03-15&sortBy=publishedAt&apiKey=a757074d6a164334a170b27bb8a57562")
+    .then((data)=>setArticle(data.data.articles))
+  },[])
   return (
     <div>
       <h2>total News : {article.length}</h2>
